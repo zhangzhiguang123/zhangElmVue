face329ec3ef02e180946ed9b927905550b40ca6
Yuer <1293792992@qq.com>
2019-11-01 17:43:17 +0800

    qjixue

--- src/api/adminsison.js
+++ src/api/adminsison.js
@@ -1,4 +1,14 @@
 import axios from "axios"
+
+axios.defaults.baseURL = "https://elm.cangdu.org/";
+
+axios.interceptors.response.use(function (response) {
+    return response.data;
+  }, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
+
 export function getAdminIin(){
-    return axios.get("https://elm.cangdu.org/admin/all?offset=0&limit=100");
+    return axios.get("admin/all?offset=0&limit=100");
 }
--- src/api/foodlist.js
+++ src/api/foodlist.js
@@ -1,4 +1,14 @@
 import axios from "axios"
-export function getFoodlist(){
-    return axios.get("https://elm.cangdu.org/shopping/v2/foods?offset=0&limit=50&restaurant_id=2")
+axios.defaults.baseURL = "https://elm.cangdu.org/";
+
+axios.interceptors.response.use(function (response) {
+    return response.data;
+}, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+});
+
+
+export function getFoodlist() {
+    return axios.get("shopping/v2/foods?offset=0&limit=50&restaurant_id=2")
 }
